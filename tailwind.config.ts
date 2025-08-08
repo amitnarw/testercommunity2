5f4ea76768577766cf6725fcadf45b7fb7dc8ec8
Amit Narwal <amitnarwal114@gmail.com>
2025-08-08 12:42:42 +0000

    increase speed of "Trusted by a Community of Innovators" section to 3 ti

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -111,8 +111,8 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'scroll-ltr': 'scroll-ltr 60s linear infinite',
-        'scroll-rtl': 'scroll-rtl 60s linear infinite',
+        'scroll-ltr': 'scroll-ltr 20s linear infinite',
+        'scroll-rtl': 'scroll-rtl 20s linear infinite',
         'fade-in-up': 'fade-in-up 0.8s ease-out forwards',
         'image-zoom': 'image-zoom 0.5s ease-in-out forwards',
       },
