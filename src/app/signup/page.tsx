60569f52c720cd1d9a3c69680574f06715d9af02
Amit Narwal <amitnarwal114@gmail.com>
2025-08-12 10:52:24 +0000

    remove the div around the register page flex items-center justify-cente

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -19,7 +19,6 @@ export default function SignupPage() {
             </p>
         </div>
       </div>
-      <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
             <div>
               <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-foreground">
@@ -35,7 +34,6 @@ export default function SignupPage() {
                 </CardContent>
             </Card>
         </div>
-      </div>
     </div>
   );
 }
