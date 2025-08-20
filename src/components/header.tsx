d760597510c9af78d0ed8d605b25d2bfa7f50299
Amit Narwal <amitnarwal114@gmail.com>
2025-08-20 06:57:11 +0000

    nope, I don't like it at all, you jsut have to be creative and open mind

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -148,166 +148,168 @@ export function Header({
   if (isAuthPage) return null;
 
   return (
-    <header className="sticky top-3 z-30 w-[90vw] m-auto rounded-full transition-all duration-300 bg-gradient-to-r from-primary/50 to-secondary/30 dark:from-primary/15 dark:to-secondary/15 backdrop-blur-lg pl-2 py-2">
-      <div className="container mx-auto px-2">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-2">
-            {isDashboardPage ? (
-              <div className="flex items-center gap-2">
-                <Button size="icon" variant="ghost" onClick={() => setMobileOpen(!isMobileMenuOpen)} className="md:hidden">
-                  <Menu className="h-6 w-6" />
-                  <span className="sr-only">Open menu</span>
-                </Button>
-                <Button variant="ghost" size="icon" onClick={() => setSidebarCollapsed(!isSidebarCollapsed)} className="hidden md:flex">
-                  {isSidebarCollapsed ? <PanelLeftOpen /> : <PanelLeftClose />}
-                  <span className="sr-only">{isSidebarCollapsed ? "Expand sidebar" : "Collapse sidebar"}</span>
-                </Button>
-              </div>
-            ) : (
-              <Link href="/">
-                <InTestersLogoShortHeader className="h-10 w-10" />
-              </Link>
-            )}
-          </div>
-
-          {!isDashboardPage && (
-            <nav className="hidden md:flex items-center gap-6">
-              {navItems.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  data-text={item.name}
-                  className={cn(
-                    'font-medium transition-colors sliding-text-hover',
-                    (pathname.startsWith(item.href) && item.href !== '/') || (pathname === '/' && item.href === '/') ? 'text-primary' : 'text-muted-foreground'
-                  )}
-                >
-                  <span>{item.name}</span>
-                </Link>
-              ))}
-            </nav>
-          )}
-
-          <div className="flex items-center gap-2">
-            {isMounted && (
-              <>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-                >
-                  <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Toggle theme</span>
-                </Button>
-
-                {isAuthenticated ? (
-                  <div className="hidden md:block">
-                    <UserNav onLogout={onLogout} />
-                  </div>
-                ) : (
-                  <div className="hidden md:flex items-center gap-2">
-                    <Button variant="ghost" asChild>
-                      <Link href="/login">Log In</Link>
-                    </Button>
-                    <Button asChild>
-                      <Link href="/signup">Sign Up <ArrowRight className="ml-2 h-4 w-4" /></Link>
-                    </Button>
-                  </div>
-                )}
-              </>
-            )}
-
-            <div className="md:hidden">
-              <Sheet open={isVisitorMenuOpen} onOpenChange={setVisitorMenuOpen}>
-                <SheetTrigger asChild>
-                  <Button size="icon" variant="ghost">
+    <header className="sticky top-4 z-50 w-[95vw] mx-auto">
+      <div className="bg-transparent backdrop-blur-lg rounded-full border border-black/5 dark:border-white/5 shadow-inner shadow-black/5 dark:shadow-white/5 transition-all duration-300">
+        <div className="container mx-auto px-4 py-2">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              {isDashboardPage ? (
+                <div className="flex items-center gap-2">
+                  <Button size="icon" variant="ghost" onClick={() => setMobileOpen(!isMobileMenuOpen)} className="md:hidden">
                     <Menu className="h-6 w-6" />
                     <span className="sr-only">Open menu</span>
                   </Button>
-                </SheetTrigger>
-                <SheetContent side="top" className="flex flex-col h-full">
-                  <SheetHeader>
-                    <div className="flex justify-between items-center">
+                  <Button variant="ghost" size="icon" onClick={() => setSidebarCollapsed(!isSidebarCollapsed)} className="hidden md:flex">
+                    {isSidebarCollapsed ? <PanelLeftOpen /> : <PanelLeftClose />}
+                    <span className="sr-only">{isSidebarCollapsed ? "Expand sidebar" : "Collapse sidebar"}</span>
+                  </Button>
+                </div>
+              ) : (
+                <Link href="/">
+                  <InTestersLogoShortHeader className="h-10 w-10" />
+                </Link>
+              )}
+            </div>
+
+            {!isDashboardPage && (
+              <nav className="hidden md:flex items-center gap-6">
+                {navItems.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    data-text={item.name}
+                    className={cn(
+                      'font-medium transition-colors sliding-text-hover',
+                      (pathname.startsWith(item.href) && item.href !== '/') || (pathname === '/' && item.href === '/') ? 'text-primary' : 'text-muted-foreground'
+                    )}
+                  >
+                    <span>{item.name}</span>
+                  </Link>
+                ))}
+              </nav>
+            )}
+
+            <div className="flex items-center gap-2">
+              {isMounted && (
+                <>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                  >
+                    <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Toggle theme</span>
+                  </Button>
+
+                  {isAuthenticated ? (
+                    <div className="hidden md:block">
+                      <UserNav onLogout={onLogout} />
+                    </div>
+                  ) : (
+                    <div className="hidden md:flex items-center gap-2">
+                      <Button variant="ghost" asChild>
+                        <Link href="/login">Log In</Link>
+                      </Button>
+                      <Button asChild>
+                        <Link href="/signup">Sign Up <ArrowRight className="ml-2 h-4 w-4" /></Link>
+                      </Button>
+                    </div>
+                  )}
+                </>
+              )}
+
+              <div className="md:hidden">
+                <Sheet open={isVisitorMenuOpen} onOpenChange={setVisitorMenuOpen}>
+                  <SheetTrigger asChild>
+                    <Button size="icon" variant="ghost">
+                      <Menu className="h-6 w-6" />
+                      <span className="sr-only">Open menu</span>
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent side="top" className="flex flex-col h-full">
+                    <SheetHeader>
+                      <div className="flex justify-between items-center">
+                        {isAuthenticated ? (
+                          <div className="flex items-center gap-4">
+                            <Avatar className="h-12 w-12">
+                              <AvatarImage src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=400&auto=format=fit=crop" data-ai-hint="man smiling" alt="User Avatar" />
+                              <AvatarFallback>{demoUser.role.charAt(0).toUpperCase()}</AvatarFallback>
+                            </Avatar>
+                            <div className='flex flex-col items-start justify-start'>
+                              <p className="font-bold">Demo User</p>
+                              <p className="text-sm text-muted-foreground">demo@inTesters.com</p>
+                            </div>
+                          </div>
+                        ) : (
+                          <div></div>
+                        )}
+                        <SheetClose asChild>
+                          <Button size="icon" variant="ghost">
+                            <X className="h-6 w-6" />
+                            <span className="sr-only">Close menu</span>
+                          </Button>
+                        </SheetClose>
+                      </div>
+                    </SheetHeader>
+                    <div className="p-6 flex flex-col justify-between flex-1">
                       {isAuthenticated ? (
-                        <div className="flex items-center gap-4">
-                          <Avatar className="h-12 w-12">
-                            <AvatarImage src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=400&auto=format=fit=crop" data-ai-hint="man smiling" alt="User Avatar" />
-                            <AvatarFallback>{demoUser.role.charAt(0).toUpperCase()}</AvatarFallback>
-                          </Avatar>
-                          <div className='flex flex-col items-start justify-start'>
-                            <p className="font-bold">Demo User</p>
-                            <p className="text-sm text-muted-foreground">demo@inTesters.com</p>
+                        <div className="flex flex-col h-full">
+                          <nav className="flex flex-col items-center text-center justify-center gap-6 h-full">
+                            {mobileAuthenticatedNavItems.map((item) => (
+                              <Link
+                                key={item.name}
+                                href={item.href}
+                                onClick={() => setVisitorMenuOpen(false)}
+                                className={cn(
+                                  'text-2xl font-medium transition-colors hover:text-primary',
+                                  pathname === item.href ? 'text-primary' : 'text-foreground'
+                                )}
+                              >
+                                {item.name}
+                              </Link>
+                            ))}
+                          </nav>
+                          <div className="flex justify-center items-center gap-2">
+                            <Button variant="ghost" asChild onClick={() => setVisitorMenuOpen(false)} className='w-full border'>
+                              <Link href="/profile">Profile</Link>
+                            </Button>
+                            <Button variant="ghost" onClick={() => { onLogout(); setVisitorMenuOpen(false); }} className='bg-red-500/20 w-full'>
+                              Log Out
+                            </Button>
                           </div>
                         </div>
                       ) : (
-                        <div></div>
+                        <>
+                          <nav className="flex flex-col items-center text-center gap-8 mt-8">
+                            {visitorNavItems.map((item) => (
+                              <Link
+                                key={item.name}
+                                href={item.href}
+                                onClick={() => setVisitorMenuOpen(false)}
+                                className={cn(
+                                  'text-2xl font-medium transition-colors hover:text-primary',
+                                  pathname === item.href ? 'text-primary' : 'text-foreground'
+                                )}
+                              >
+                                {item.name}
+                              </Link>
+                            ))}
+                          </nav>
+                          <div className="mt-auto flex flex-col gap-4">
+                            <Button variant="outline" size="lg" asChild onClick={() => setVisitorMenuOpen(false)}>
+                              <Link href="/login">Log In</Link>
+                            </Button>
+                            <Button asChild size="lg" onClick={() => setVisitorMenuOpen(false)}>
+                              <Link href="/signup">Sign Up</Link>
+                            </Button>
+                          </div>
+                        </>
                       )}
-                      <SheetClose asChild>
-                        <Button size="icon" variant="ghost">
-                          <X className="h-6 w-6" />
-                          <span className="sr-only">Close menu</span>
-                        </Button>
-                      </SheetClose>
                     </div>
-                  </SheetHeader>
-                  <div className="p-6 flex flex-col justify-between flex-1">
-                    {isAuthenticated ? (
-                      <div className="flex flex-col h-full">
-                        <nav className="flex flex-col items-center text-center justify-center gap-6 h-full">
-                          {mobileAuthenticatedNavItems.map((item) => (
-                            <Link
-                              key={item.name}
-                              href={item.href}
-                              onClick={() => setVisitorMenuOpen(false)}
-                              className={cn(
-                                'text-2xl font-medium transition-colors hover:text-primary',
-                                pathname === item.href ? 'text-primary' : 'text-foreground'
-                              )}
-                            >
-                              {item.name}
-                            </Link>
-                          ))}
-                        </nav>
-                        <div className="flex justify-center items-center gap-2">
-                          <Button variant="ghost" asChild onClick={() => setVisitorMenuOpen(false)} className='w-full border'>
-                            <Link href="/profile">Profile</Link>
-                          </Button>
-                          <Button variant="ghost" onClick={() => { onLogout(); setVisitorMenuOpen(false); }} className='bg-red-500/20 w-full'>
-                            Log Out
-                          </Button>
-                        </div>
-                      </div>
-                    ) : (
-                      <>
-                        <nav className="flex flex-col items-center text-center gap-8 mt-8">
-                          {visitorNavItems.map((item) => (
-                            <Link
-                              key={item.name}
-                              href={item.href}
-                              onClick={() => setVisitorMenuOpen(false)}
-                              className={cn(
-                                'text-2xl font-medium transition-colors hover:text-primary',
-                                pathname === item.href ? 'text-primary' : 'text-foreground'
-                              )}
-                            >
-                              {item.name}
-                            </Link>
-                          ))}
-                        </nav>
-                        <div className="mt-auto flex flex-col gap-4">
-                          <Button variant="outline" size="lg" asChild onClick={() => setVisitorMenuOpen(false)}>
-                            <Link href="/login">Log In</Link>
-                          </Button>
-                          <Button asChild size="lg" onClick={() => setVisitorMenuOpen(false)}>
-                            <Link href="/signup">Sign Up</Link>
-                          </Button>
-                        </div>
-                      </>
-                    )}
-                  </div>
-                </SheetContent>
-              </Sheet>
+                  </SheetContent>
+                </Sheet>
+              </div>
             </div>
           </div>
         </div>
