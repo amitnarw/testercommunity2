c0a69cae31d58455ded4934f895f4049b277cb9c
Amit Narwal <amitnarwal114@gmail.com>
2025-12-25 07:35:49 +0000

    Try fixing this error: `Runtime ReferenceError: CardFooter is not define

--- src/app/(authenticated)/wallet/page.tsx
+++ src/app/(authenticated)/wallet/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Badge } from "@/components/ui/badge";
